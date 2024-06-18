ed412bb608493df4d463260474d3ac9b9da5cc9b
doxcom <aldoperez1319@gmail.com>
2024-06-17 19:45:44 -0600

    initial files and pom

---
+++ src/test/java/aldoperez/springframework/spring5_jokes_app_v2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package aldoperez.springframework.spring5_jokes_app_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
