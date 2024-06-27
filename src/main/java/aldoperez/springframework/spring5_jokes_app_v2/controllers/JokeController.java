546b1e8c1b9d3c3fb742066392d2a70927c451b7
doxcom <aldoperez1319@gmail.com>
2024-06-27 00:14:04 -0600

    joke controller created

---
+++ src/main/java/aldoperez/springframework/spring5_jokes_app_v2/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package aldoperez.springframework.spring5_jokes_app_v2.controllers;
+
+
+import aldoperez.springframework.spring5_jokes_app_v2.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    //spring will do automatically dependency injection instead of use @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
+
